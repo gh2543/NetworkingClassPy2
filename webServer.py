20f21ae3e2c47aaacd5ee73ed7e3d73daeda8e84
gh2543 <gh2543@nyu.edu>
2025-06-12 16:49:25 -0400

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -36,9 +36,9 @@ def webServer(port=13331):
       #This variable can store the headers you want to send for any valid or invalid request.   What header should be sent for a response that is ok?    
       #Fill in start*********** 
               
-      #Content-Type is an example on how to send a header as bytes. There are more!; charset=UTF-8
+      #Content-Type is an example on how to send a header as bytes. There are more!
 
-      outputdata = b"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
+      outputdata = b"HTTP/1.1 200 OK\r\nConnection: close\r\nServer: Apache/2.2.3 (CentOS)\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
       #header = b"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
       #connectionSocket.send(header)
       #Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
